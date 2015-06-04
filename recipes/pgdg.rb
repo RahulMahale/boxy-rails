626056c5ed777e517523c5c19769d763e74e1b5e
Sahil Muthoo <sahil.muthoo@gmail.com>
2015-06-04 13:04:44 +0530

    Fix #6 - Make createuser idempotent

--- recipes/pgdg.rb
+++ recipes/pgdg.rb
@@ -1,9 +1,8 @@
 include_recipe 'postgresql::server'
 include_recipe 'postgresql::contrib'
 
-execute 'create deployer user for pg' do
-  command "createuser #{node['boxy-rails']['deployer']} -d -i"
+execute "create #{node['boxy-rails']['deployer']} user for pg" do
   user 'postgres'
-  ignore_failure true
-  action :run
+  command "createuser #{node['boxy-rails']['deployer']} -d -i"
+  not_if %(psql postgres -tAc "SELECT 1 FROM pg_roles WHERE rolname='#{node['boxy-rails']['deployer']}'" | grep -q 1)
 end
